ae86b3015ba1367a517696bb799b70efd4db51ac
kalyanam vijaya kumar <vijaykalyanam@gmail.com>
2023-11-21 01:08:53 +0530

    Create transport_handler.py

---
+++ transport_handler.py
@@ -0,0 +1,58 @@
+import paramiko
+
+
+class TransportHandler:
+    cmd : str = None
+    transport : paramiko.Transport = None
+    sresult: bytes = bytes(0)
+    fresult: bytes = bytes(0)
+    ret: int = -1
+
+    def __init__(self, transport: paramiko.Transport, cmd :str):
+
+        self.cmd=cmd
+        self.transport=transport
+
+        try:
+            session=self.transport.open_session()
+            self.session = session
+
+        except:
+            raise Exception("Failed to Launch session")
+
+    def poll(self):
+
+        buffSize = 4096
+
+        session=self.session
+        if session.exit_status_ready():
+            ret = session.recv_exit_status()
+            if ret != 0:
+                self.fresult=session.recv_stderr(buffSize)
+            else:
+                if session.recv_ready():
+                    result = session.recv(buffSize)
+                    self.sresult = self.sresult + result
+
+            self.ret=ret
+            return True
+
+        if session.recv_ready():
+            result = session.recv(buffSize)
+            self.sresult = self.sresult + result
+
+        '''Command is still running.....'''
+        return False
+
+    def wait(self):
+        print("wait")
+
+    def kill(self):
+        print("kill")
+
+    def close(self):
+        print(f"close {self.cmd}")
+        self.session.close()
+
+    def run(self):
+        self.session.exec_command(self.cmd)
